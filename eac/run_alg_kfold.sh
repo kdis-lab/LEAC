df87170ce54f4c7f7808e161fec2b9adbf5a2c66
hbrobles <hbrobles@gmail.com>
2019-04-07 17:39:33 -0500

    Add files via upload

--- eac/run_alg_kfold.sh
+++ eac/run_alg_kfold.sh
@@ -12,17 +12,17 @@
 #
 #
 #  Use:
-#       parallel -j10 ./dataset_proc.sh ::: {1..10} > log.log4 &
+#       parallel -j10 ./dataset_proc.sh ::: {1..200} > log.log4 &
 #
+#MODIFY THE PATH WHERE THE LEAC BINARY IS LOCATED
+PATH_ALGORITHMS="/home/hermes/leac-master/bin"
 #
-#MODIFIQUE Y ESCRIBIR EN DONDE SE ENCUENTRAN LOS BINARIOS 
-PATH_AALGORITHMS="~/leac-update/leac-master/eac"
-#MODIFIQUE Y ESCRIBA DONDE SEENECUENTRAN LOS 
+#MODIFY THE PATH WHERE THE DATASET ARE LOCATED 
 PATH_DATASETS="~/leac-update/leac-master/data"
-#PATH_DATASETS="~/dataset"
-#
-#-b  --format-file[=NAME]    uci, or keel, by default uci
-ADATASET=(
+
+#INCLUDE DATASET TO PROCESS WITH ITS CHARACTERISTICS
+# DESCRIBED BY THE PARAMETERS
+DATASET_ARRAY=(
 "iris_z5"        "-b uci -h yes -a \"1-4\"  -c 5  --number-clusters=3"
 "wine_z5"        "-b uci -h yes -a \"1-12\" -c 13 --number-clusters=3"
 "sonar_z5"       "-b uci -h yes -a \"1-60\" -c 61 --number-clusters=2"
@@ -34,33 +34,48 @@ ADATASET=(
 "hayes_roth_z5"  "-b uci -h yes -a \"1-3\"  -c 4  --number-clusters=3"
 "zoo_z5"         "-b uci -h yes -a \"1-16\" -c 17  --number-clusters=7"
 )
-#
-#NAME ALGORITHMS AND PARAMETERS OF THE ALGORITHMS
-AALGORITHMS=( 
+#WHERE LOS PARAMETROS LOS MISMOS UTILIZADOS 
+#     POR LOS METODOS DE LEAC:
+# -b  --format-file[=NAME]   
+#       uci, or keel, by default uci
+# -h, --with-header[=yes/no] 
+#       file contains names of instances or a header, 
+#       by default is no
+# -a, --select-attributes[=ARG]
+#       select the attributes to be processed for example, 
+#       "1-2,4" by default all. Also used to specify the  
+#       number of dimensions of the instances, unless 
+#       specified file is obtained instances
+# -d, --delimit-attributes=[ARG]
+#       separated file by default ","
+# -c, --class-column[=NUMBER] input file of instances 
+#       has a class assigned in the column [NUMBER=undefined]
+
+                    
+#INCLUDES ALGORITHMS AND PARAMETERS
+ALGORITHMS_ARRAY=( 
 "kga_fkcentroid"  ""
 "fgka_fklabel"    ""
 "hka_fkmedoid"    ""
 "gka_fklabel"     ""
 "gaclustering_fklabel"  ""
 "gagr_fkcentroid" "--generations=200"
-#"gas_fkcentroid"  ""
-#"igka_fklabel"    ""
-#"gca_fkmedoid"    ""
-#"gaclustering_fkcrispmatrix" "--generations=200" 
 )
+
+
 # get number of dataset
-NUM_DATASET=${#ADATASET[@]}
+NUM_DATASET=${#DATASET_ARRAY[@]}
 # get number algorithms
-NUM_ALGORITHMS=${#AALGORITHMS[@]}
+NUM_ALGORITHMS=${#ALGORITHMS_ARRAY[@]}
 #
 #
 echo "10-FOLD*************************************************************************"
 echo "********************************************************************************"
 #<<<PARAMETER IN-------------------------------------------------------------
 #
-VESION_RUN="v4"
+#VESION_RUN="v4"
 #ID_MACHINE=`cat id_machine.dat`
-NUM_RUN=1
+#NUM_RUN=1
 IFILE="$1"
 KFOLD_MOD=10
 IFILEPROC=$(( IFILE % KFOLD_MOD  ))
@@ -69,9 +84,10 @@ IFILEPROC=$(( IFILEPROC == 0? KFOLD_MOD:IFILEPROC))
 #-------------------------------------------------------------------------------
 for (( j=0; j<${NUM_ALGORITHMS}; j++ ));
 do
-NAME_ALGORITHMS=${AALGORITHMS[$j]}
-PARAM_ALGORITHMS=${AALGORITHMS[$j+1]}
-DIR_OUT=$NAME_ALGORITHMS$"_"$VESION_RUN
+NAME_ALGORITHMS=${ALGORITHMS_ARRAY[$j]}
+PARAM_ALGORITHMS=${ALGORITHMS_ARRAY[$j+1]}
+DIR_OUT=$NAME_ALGORITHMS
+#DIR_OUT=$NAME_ALGORITHMS$"_"$VESION_RUN
 #
 if [ ! -d $DIR_OUT ]; then
 echo $DIR_OUT "File " $DIR_OUT " not exists"
@@ -86,31 +102,32 @@ echo $NAME_ALGORITHMS
 #
 for (( i=0; i<${NUM_DATASET}; i++ ));
 do
-NAME_PROCESING=${ADATASET[$i]}
-PARAM_DATASET=${ADATASET[$i+1]}
-NAME_FILE=${ADATASET[$i]}"-10-"
+NAME_PROCESING=${DATASET_ARRAY[$i]}
+PARAM_DATASET=${DATASET_ARRAY[$i+1]}
+NAME_FILE=${DATASET_ARRAY[$i]}"-10-"
 #DIR_OUT=$NAME_ALGORITHMS$VESION_RUN$NAME_PROCESING
 #DIR_OUT=$NAME_ALGORITHMS$"_"$VESION_RUN
 DIR_DATASET=$PATH_DATASETS"/"$NAME_PROCESING
 echo "DIR_DATASET = " $DIR_DATASET
 
-for IRUN in $(seq 1 $NUM_RUN)
-do
+#for IRUN in $(seq 1 $NUM_RUN)
+#do
 NUMHAVE=0
 #PREFIXFILE=$NAME_ALGORITHMS"_"$NAME_PROCESING"_"$ID_MACHINE$IFILE$IRUN
-PREFIXFILE=$NAME_ALGORITHMS"_"$NAME_PROCESING"_"$IFILE"_"$IRUN
+PREFIXFILE=$NAME_ALGORITHMS"_"$NAME_PROCESING"_"$IFILE
+#PREFIXFILE=$NAME_ALGORITHMS"_"$NAME_PROCESING"_"$IFILE"_"$IRUN
 if [ -e $PREFIXFILE"_run.csv" ]; then
 NUMHAVE=$(grep "_inout,out" $PREFIXFILE"_run.csv" |  wc -l)
 fi
 if [ "$NUMHAVE" -eq "0" ]; then
 FILE_PROC=$DIR_DATASET"/"$NAME_FILE$IFILEPROC
-comando="$PATH_AALGORITHMS/$NAME_ALGORITHMS -i $FILE_PROC\"tra.dat\" -t $FILE_PROC\"tst.dat\" $PARAM_DATASET  $PARAM_ALGORITHMS -r 1 -R $PREFIXFILE\"_run.csv\" -C $PREFIXFILE\"_centroids.dat\" -M $PREFIXFILE\"_membership.dat\" -T $PREFIXFILE\"_partitionstable.dat\"&"
-#comando="$PATH_AALGORITHMS/$NAME_ALGORITHMS -i $FILE_PROC\"tra.dat\" -t $FILE_PROC\"tst.dat\" $PARAM_DATASET  $PARAM_ALGORITHMS -b keel -r 1  -q -R $PREFIXFILE\"_run.csv\" -C $PREFIXFILE\"_centroids.dat\" -M $PREFIXFILE\"_membership.dat\" -T $PREFIXFILE\"_partitionstable.dat\" --gnuplot=$PREFIXFILE&"
+comando="$PATH_ALGORITHMS/$NAME_ALGORITHMS -i $FILE_PROC\"tra.dat\" -t $FILE_PROC\"tst.dat\" $PARAM_DATASET  $PARAM_ALGORITHMS -r 1 -R $PREFIXFILE\"_run.csv\" -C $PREFIXFILE\"_centroids.dat\" -M $PREFIXFILE\"_membership.dat\" -T $PREFIXFILE\"_partitionstable.dat\"&"
+#comando="$PATH_ALGORITHMS_ARRAY/$NAME_ALGORITHMS -i $FILE_PROC\"tra.dat\" -t $FILE_PROC\"tst.dat\" $PARAM_DATASET  $PARAM_ALGORITHMS -b keel -r 1  -q -R $PREFIXFILE\"_run.csv\" -C $PREFIXFILE\"_centroids.dat\" -M $PREFIXFILE\"_membership.dat\" -T $PREFIXFILE\"_partitionstable.dat\" --gnuplot=$PREFIXFILE&"
 echo $comando
 eval $comando
 wait
 fi
-done
+#done
 ((i++))
 done #DATASET
 cd ..
